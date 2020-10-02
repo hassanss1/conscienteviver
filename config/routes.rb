64278a2f49e6b2c6e4ee2ee0bb4af5b3b65c9aa8
hassanssaade <hassanssaade@gmail.com>
2020-10-02 18:38:25 -0300

    added more blocks of the website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/linkbio' to: 'pages#linkbio'
-  get '/mentoria_acenda_a_luz' to: 'pages#mentoria_acenda_a_luz'
+  get '/linkbio', to: 'pages#linkbio'
+  get '/mentoria_acenda_a_luz', to: 'pages#mentoria_acenda_a_luz'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
