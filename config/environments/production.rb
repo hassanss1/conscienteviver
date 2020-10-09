52c9000c8b636f01e875ada4963c8bc143ce583a
hassanssaade <hassanssaade@gmail.com>
2020-10-09 14:49:38 -0300

    precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  APPLICATIONNAME::Application.configure do
-       config.assets.compile = true
-  end
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -29,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
