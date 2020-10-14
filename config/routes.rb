e7e323babf8e78ffc0107f856e38577003c49784
hassanssaade <hassanssaade@gmail.com>
2020-10-14 18:32:35 -0300

    changed root to linkbio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#mentoria_acenda_a_luz'
-  get '/linkbio', to: 'pages#linkbio'
+  # root to: 'pages#mentoria_acenda_a_luz'
+  root to: 'pages#linkbio'
+  # get '/linkbio', to: 'pages#linkbio'
   get '/details', to: 'pages#details'
   get '/transferencia', to:'pages#transferencia'
   get '/nos', to:'pages#nos'
