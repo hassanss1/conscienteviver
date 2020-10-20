f7dca46e64e540a622a0e99115b568266fab164f
hassanssaade <hassanssaade@gmail.com>
2020-10-20 11:48:31 -0300

    updated links

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   get '/transferencia', to:'pages#transferencia'
   get '/nos', to:'pages#nos'
   # Routes for mentoria_acenda_luz
-  devise_for :users, path: '/mentoria_acenda_a_luz', path_names: { sign_in: 'login', sign_out: 'logout'}
-  get '/mentoria_acenda_a_luz/home', to: 'pages#mentoria_acenda_a_luz_home'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  get '/home', to: 'pages#mentoria_acenda_a_luz_home'
 
   # Routes for aulas
-  get '/mentoria_acenda_a_luz/aulas/1', to: 'pages#aula1', as: 'aula1'
+  get '/aulas/1', to: 'pages#aula1', as: 'aula1'
   # get '/mentoria_acenda_a_luz/aulas/2', to: 'pages#aula2', as: 'aula2'
   # get '/mentoria_acenda_a_luz/aulas/3', to: 'pages#aula3', as: 'aula3'
   # get '/mentoria_acenda_a_luz/aulas/4', to: 'pages#aula4', as: 'aula4'
